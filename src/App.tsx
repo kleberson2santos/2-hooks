dcefeee25d27f8ec6d02009f5d915661e3d68586
Kleberson Santos <kleberson2santos@gmail.com>
2021-08-03 15:18:07 -0400

    como realmente funciona o hook useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useEffect, useRef } from 'react';
 import './App.css';
 import PeopleList from './hooks/PeopleList';
 import useCounter from './hooks/useCounter';
 
 function App() {
   const number = useCounter(9);
+  // const counter = { current: 2 };
+  const counter = useRef(2);
+  const div = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //   counter.current = 5;
+    //   console.log('o valor de counter foi alterado');
+    // }, 2000);
+
+    console.log(div);
+
+    if (div.current) {
+      div.current.style.backgroundColor = '#09f';
+    }
+  }, []);
+
+  // counter.current = 5;
+
   return (
-    <div className='App'>
+    <div className='App' ref={div}>
       {number}
+      <div style={{ backgroundColor: 'peachpuff' }}>{counter.current}</div>
       <PeopleList />
       <PeopleList />
     </div>
